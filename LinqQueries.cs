7d002c359361b1972718d1a5653b0f173bb5b0de
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-07-02 15:43:53 -0400

    ✨ Refactor PrintBooks method visibility and update book retrieval methods for clarity

--- LinqQueries.cs
+++ LinqQueries.cs
@@ -19,7 +19,7 @@ public class LinqQueries
         PrintBooks(booksCollection);
     }
 
-    public static void PrintBooks(IEnumerable<Book> books)
+    private static void PrintBooks(IEnumerable<Book> books)
     {
         string textFormat = "{0, -60} {1, 15} {2, 20}";
         Console.WriteLine($"{textFormat}\n", "Title", "Pages", "Published Date");
@@ -31,8 +31,28 @@ public class LinqQueries
 
     public void GetBooksAfterYear(int year)
     {
-        PrintBooks(booksCollection.Where(book => book.PublishedDate.Year > year));
+        // Extension method
+        IEnumerable<Book> books = booksCollection.Where(book => book.PublishedDate.Year > year);
+
+        // Query method
+        // IEnumerable<Book> books = from book in booksCollection
+        //                             where book.PublishedDate.Year > year
+        //                             select book;
+
+        PrintBooks(books);
     }
 
+    public void GetBooksWithSpecificTitleAndPageCount(string title, int pageCount)
+    {
+        // Extension method
+        IEnumerable<Book> books = booksCollection.Where(
+            book => book.Title.Contains(title, StringComparison.OrdinalIgnoreCase) && book.PageCount > pageCount);
 
+        // Query method
+        // IEnumerable<Book> books = from book in booksCollection
+        //                                                 where book.Title.Contains(title, StringComparison.OrdinalIgnoreCase) && book.PageCount > pageCount
+        //                                                 select book;
+
+        PrintBooks(books);
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 LinqQueries queries = new();
 
 // Get all books
-queries.GetAllBooks();
+// queries.GetAllBooks();
 
 // Get books published after a specific year
-queries.GetBooksAfterYear(2010);
+// queries.GetBooksAfterYear(2010);
+
+// Get books with a specific title and page count
+// queries.GetBooksWithSpecificTitleAndPageCount("android", 200);
