30a4528906feec0d2620ea8ea30f898a7e53cb03
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-07-02 15:02:46 -0400

    ✨ Update book retrieval methods to use correct year and improve printing functionality

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 LinqQueries queries = new();
 
 // Get all books
-queries.PrintBooks();
+queries.GetAllBooks();
 
 // Get books published after a specific year
-queries.GetBooksAfterYear(2000);
+queries.GetBooksAfterYear(2010);
